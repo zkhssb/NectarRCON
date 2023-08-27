0877d862deff73e20fc4eb930d90725122847f71
zkhssb <z164907681@gmail.com>
2023-08-27 11:05:21 +0800

    add: DNS单元测试

---
+++ NectarRCON.Tests/DNSHelperTests.cs
@@ -0,0 +1,34 @@
+﻿using NectarRCON.Core.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace NectarRCON.Tests
+{
+    [TestClass]
+    public class DNSHelperTests
+    {
+        /// <summary>
+        /// A解析测试
+        /// </summary>
+        [TestMethod]
+        public void ATest()
+        {
+            string? ip = DNSHelpers.AQuery("hypixel.net");
+            Console.WriteLine(ip);
+            Assert.IsNotNull(string.IsNullOrEmpty(ip));
+        }
+
+        /// <summary>
+        /// SRV解析测试
+        /// </summary>
+        [TestMethod]
+        public void SRVTest()
+        {
+            string? ip = DNSHelpers.SRVQuery("mc.125nf.com");
+            Console.WriteLine(ip);
+            Assert.IsFalse(string.IsNullOrEmpty(ip));
+        }
+    }
+}
