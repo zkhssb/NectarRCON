7e7607426ffdf3d4c4d39bd6c98e7d46a1ae34e4
zkhssb <z164907681@gmail.com>
2023-08-27 19:48:46 +0800

    add: 一些简单的转换器

---
+++ NectarRCON/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows;
+using System.Globalization;
+
+namespace NectarRCON.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is bool boolValue)
+            {
+                return boolValue ? Visibility.Visible : Visibility.Hidden;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NectarRCON/Converters/IntToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows;
+using System.Globalization;
+
+namespace NectarRCON.Converters
+{
+    public class IntToVisibilityConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is int intValue)
+            {
+                return intValue <= 0 ? Visibility.Visible : Visibility.Hidden;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
