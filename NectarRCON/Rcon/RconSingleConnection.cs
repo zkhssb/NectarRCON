a3a89caf7fa02f6ae6bdc251d48d7d1061919833
zkhssb <z164907681@gmail.com>
2024-01-26 00:43:09 +0800

    add: Rcon相关页面与服务增加日志

--- NectarRCON/Rcon/RconSingleConnection.cs
+++ NectarRCON/Rcon/RconSingleConnection.cs
@@ -9,26 +9,21 @@ using System.Net.Sockets;
 using System.Security.Authentication;
 using System.Windows;
 using NectarRCON.Dp;
+using Serilog;
 
 namespace NectarRCON.Services;
-public class RconSingleConnection : IRconConnection
+public class RconSingleConnection(
+    IServerPasswordService serverPasswordService,
+    ILanguageService languageService,
+    IRconConnectionInfoService rconConnectionInfoService)
+    : IRconConnection
 {
     private readonly RconSettingsDp _settingsDp = DpFile.LoadSingleton<RconSettingsDp>();
-    private readonly IServerPasswordService _serverPasswordService;
-    private readonly ILanguageService _languageService;
-    private readonly IRconConnectionInfoService _rconConnectionInfoService;
-
-    public RconSingleConnection(IServerPasswordService serverPasswordService, ILanguageService languageService, IRconConnectionInfoService rconConnectionInfoService)
-    {
-        _serverPasswordService = serverPasswordService;
-        _languageService = languageService;
-        _rconConnectionInfoService = rconConnectionInfoService;
-    }
 
     private IRconAdapter? _rconClient
         = null;
 
-    private bool _connecting = false;
+    private bool _connecting;
 
     public event MessageEvent? OnMessage;
     public event RconEvent? OnClosed;
@@ -51,7 +46,7 @@ public class RconSingleConnection : IRconConnection
     }
     public void Connect()
     {
-        ServerInformation info = _rconConnectionInfoService.GetLastInformation() ?? throw new ArgumentNullException("Internal error, please try again");
+        ServerInformation info = rconConnectionInfoService.GetLastInformation() ?? throw new ArgumentNullException("Internal error, please try again");
         _connecting = true;
         OnConnecting?.Invoke(info);
         try
@@ -64,7 +59,7 @@ public class RconSingleConnection : IRconConnection
                 address = $"{info.Address.Replace("localhost", "127.0.0.1")}:{info.Port}";
 
 
-            ServerPassword? serverPassword = _serverPasswordService.Get(info);
+            ServerPassword? serverPassword = serverPasswordService.Get(info);
             string password = serverPassword?.Password ?? string.Empty;
 
             // 创建对应的Rcon客户端实例
@@ -72,9 +67,11 @@ public class RconSingleConnection : IRconConnection
             _rconClient = AdapterHelpers.CreateAdapterInstance(info.Adapter)
                 ?? throw new InvalidOperationException($"adapter not found: {info.Adapter}");
             _rconClient.SetEncoding(_settingsDp.Encoding.GetEncoding());
-            string host = address.Split(":")[0];
-            int port = int.Parse(address.Split(":")[1]);
+            var host = address.Split(":")[0];
+            var port = int.Parse(address.Split(":")[1]);
 
+            Log.Information("[RconSingleConnection] Connecting to {host}:{port}", host, port);
+            
             _rconClient.Connect(host, port); // 连接
 
             if (!_rconClient.Authenticate(password))
@@ -86,8 +83,8 @@ public class RconSingleConnection : IRconConnection
         }
         catch (FormatException ex)
         {
-            MessageBox.Show(_languageService.GetKey("ui.text.format_exception")
-                .Replace("%s", ex.Message), _languageService.GetKey("text.error"), MessageBoxButton.OK, MessageBoxImage.Error);
+            MessageBox.Show(languageService.GetKey("ui.text.format_exception")
+                .Replace("%s", ex.Message), languageService.GetKey("text.error"), MessageBoxButton.OK, MessageBoxImage.Error);
         }
         finally
         {
@@ -113,36 +110,39 @@ public class RconSingleConnection : IRconConnection
                 Close();
                 if (ex is SocketException or IOException && _settingsDp.AutoReconnect)
                 {
+                    Log.Information("Rcon连接已经断开,开始自动重连...");
                     try
                     {
                         Connect();
                     }
-                    catch
+                    catch (Exception e)
                     {
-                        // ignored
+                        Log.Error("自动重连失败 {err}", e);
                     }
 
                     if (IsConnected())
                     {
                         try
                         {
+                            Log.Information("尝试重发命令...");
                             string result = _rconClient.Run(command);
                             OnMessage?.Invoke(_serverInformation, result);
+                            Log.Information("重发命令成功 -> {command}", command);
                             return;
                         }
-                        catch
+                        catch(Exception e)
                         {
-                            // ignored
+                            Log.Error("尝试重发命令失败 {err}", e);
                         }
                     }
                 }
-                MessageBox.Show($"{_languageService.GetKey("text.error")}\n{ex.Message}", ex.GetType().FullName, MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show($"{languageService.GetKey("text.error")}\n{ex.Message}", ex.GetType().FullName, MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
         else
         {
             Close();
-            MessageBox.Show($"{_languageService.GetKey("text.server.not_connect.text")}", _languageService.GetKey("text.error"), MessageBoxButton.OK, MessageBoxImage.Error);
+            MessageBox.Show($"{languageService.GetKey("text.server.not_connect.text")}", languageService.GetKey("text.error"), MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
